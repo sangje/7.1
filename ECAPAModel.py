1d49eb7fc301e4c578cb4eb781fff4296b8f06d1
sangje <parksj2480@naver.com>
2023-10-12 11:45:10 +0900

    data_list doesn't exist? > self deleted

--- ECAPAModel.py
+++ ECAPAModel.py
@@ -67,9 +67,9 @@ class ECAPAModel(nn.Module):
 			file_name     = os.path.join(test_path, line.split()[0]+'_AUDIO.wav') # Convert 301 > ~301_AUDIO.wav
 			data_label.append(speaker_label)
 			data_list.append(file_name)
-		print(data_list)
+		
 		for i, data in enumerate(data_list):
-			audio, sr = soundfile.read(self.data_list[i])
+			audio, sr = soundfile.read(data_list[i])
 			if len(audio.shape) == 2 and audio.shape[1] > 1:
 				audio = audio[:, 0]  # Extract the left channel
 			prediction.append(self.model(audio).item())
